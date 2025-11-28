4ef0d003c5fc2410d995ac3803f2e35543af9529
jarosmat <matej.jaros@centrum.cz>
2025-11-28 22:41:40 +0100

    Add first implementation of CNFEncoder class

---
+++ VAPL/SATsolver/lib/CNFEncoder.py
@@ -0,0 +1,37 @@
+class CNFEncoder:
+	def __init__(self, nodes, edges, edgeTuples, outputFile):
+		self.nodes = nodes
+		self.edges = edges
+		self.edgeTuples = edgeTuples
+		self.outputFile = outputFile
+
+	def encode(self):
+		i = 1
+		nodeEncoding = dict()
+		for node in self.nodes:
+			nodeEncoding[node] = i
+			i += 1
+
+		with open(self.outputFile, "w") as file:
+			n = len(self.nodes)
+			m = len(self.edgeTuples)
+			file.write(f'p cnf {n} {n + m}\n')
+
+			for edge in self.edgeTuples:
+				node1 = nodeEncoding[edge[0]]
+				node2 = nodeEncoding[edge[1]]
+
+				file.write(f'-{node1} -{node2} 0\n')
+
+			for node in self.nodes:
+				nodeEnc = nodeEncoding[node]
+				neighbors = self.edges[node]
+
+				file.write(f'{nodeEnc} ')
+
+				for n in neighbors:
+					file.write(f'{nodeEncoding[n]} ')
+
+				file.write(f'0\n')
+
+
